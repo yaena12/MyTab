728945f7746afe94b9469a0f5c8b5c6da43de602
DESKTOP-U5ST0VH\Lynn <lynna1004@gmail.com>
2017-09-08 00:13:03 +0900

    대화상자 추가

--- app/src/main/java/org/androidtown/mytab/Fragment1.java
+++ app/src/main/java/org/androidtown/mytab/Fragment1.java
@@ -1,13 +1,19 @@
 package org.androidtown.mytab;
 
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,7 +27,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * Created by Yeo on 2017-08-04.
+ * Created by Lynn on 2017-08-04.
  */
 
 public class Fragment1 extends Fragment {
@@ -31,6 +37,12 @@ public class Fragment1 extends Fragment {
 
     TextView temperaturetime; //텍스트뷰 변수
     TextView temperature;
+
+    Button button;
+    ToggleButton toggle;
+
+    ImageView imageview1 = null, imageview2 = null, imageview = null;
+
 
     private static final String TAG_RESULTS = "result";
     private static final String TAG_TIME = "time";
@@ -45,7 +57,51 @@ public class Fragment1 extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         personList = new ArrayList<HashMap<String, String>>();
+
+
+
     }
+    private void showMessage(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("환수");
+        builder.setMessage("환수를 하시겠습니까?");
+        builder.setIcon(android.R.drawable.ic_dialog_alert);
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.dismiss();
+                //String message = "예 버튼이 눌렸습니다.";
+               // textView.setText(message);
+            }
+        });
+
+        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //Toast.makeText(getApplicationContext(), "아니오 버튼이 눌렸습니다.",Toast.LENGTH_LONG).show();
+                //String message = "아니오 버튼이 눌렸습니다.";
+                //textView.setText(message);
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+
+
+ /*   private void displayTemperature(int temperature) {
+        TextView teperatureTextView = (TextView) findViewById(R.id.temperature_text_view);
+        temperatureTextView.setText("" + temperature);
+    }
+
+
+    private void displayWater(int waterLevel) {
+        TextView waterTextView = (TextView) findViewById(R.id.water_text_view);
+        temperatureTextView.setText("" + waterLevel);
+    }*/
+
+//test
 
     @Nullable
     @Override
@@ -54,6 +110,35 @@ public class Fragment1 extends Fragment {
 
         temperaturetime = rootview.findViewById(R.id.timetext);
         temperature = rootview.findViewById(R.id.temptext);
+
+        imageview = rootview.findViewById(R.id.imageView);
+
+
+        button = rootview.findViewById(R.id.waterButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                showMessage();
+            }
+        });
+
+
+        toggle = rootview.findViewById(R.id.lightButton);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    imageview.setImageResource(R.drawable.fishtankdefault);
+
+
+                    // The toggle is enabled
+                } else {
+                    imageview.setImageResource(R.drawable.fishtanklampon);
+
+                    // The toggle is disabled
+                }
+            }
+        });
+
+
 
         return rootview;
     }
